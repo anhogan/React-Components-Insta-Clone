acd3d417039fbcdd1da49cf736a37e570bfdce7f
Amanda Hogan <ahogan@teamworks.com>
2020-01-21 11:15:17 -0500

    Add like functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import dummyData from './dummy-data';
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
+  
   return (
     <div className="App">
       <SearchBar />
