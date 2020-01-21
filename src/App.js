163949ea7626ccc32f14d1ac90aac877aa70312a
Amanda Hogan <ahogan@teamworks.com>
2020-01-21 11:59:04 -0500

    Like section styling

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import dummyData from './dummy-data';
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
-  const data = useState(dummyData)
+  const post = useState(dummyData)
 
   return (
     <div className="App">
       <SearchBar />
-      <PostsPage post={data}/>
+      <PostsPage post={post}/>
       {/* Add components here  and pass props where appropriate */}
     </div>
   );
--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -15,8 +15,7 @@ const LikeSection = props => {
       </div>
     </div>
     <p className="like-number">
-      {props.likes}
-      likes</p>
+      {props.likes} likes</p>
 </div>
   )
 };
